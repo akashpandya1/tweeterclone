b5934acd1ede6c9db9853b08e0826106c05f2c20
MichaelMahon <michaelmahon92@verizon.net>
2016-09-08 13:14:59 -0400

    working on user table

--- user.db.js
+++ user.db.js
@@ -1,3 +1,37 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = sqlite3.Database('test.db');
+var db = new sqlite3.Database('test.db');
 
+function selectRec() {    
+    return new Promise(
+        (resolve, reject) => {      
+            var rows = [];     
+            db.each("SELECT * FROM User", 
+                function (err, row) {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                        return false;
+                    }
+                    rows.push(row);                    
+                },
+                function (err, nRows) {
+                    resolve(rows);
+                }
+            
+            );           
+        });
+}
+
+var p = selectRec();
+p.then(
+    (rows) => {
+         console.log("return rows: " + rows);
+         var jsonrows= JSON.stringify(rows);
+         console.log("retun json: " + jsonrows);  
+         return rows;
+    }
+).catch(
+    (err) => {
+        console.log(err);
+    }
+    );

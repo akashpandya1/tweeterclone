e61dc4f245c19db5cfe516ea15a78d3a49b8635e
akashpandya1 <akashpandya@gmail.com>
2016-09-09 12:43:10 -0400

    add web frontend

---
+++ app.js
@@ -0,0 +1,115 @@
+var express = require('express'),
+    app = express(),
+    sqlite3 = require('sqlite3').verbose(),
+    db = new sqlite3.Database('test.db'),
+    fs = require('fs'),
+    html = fs.readFileSync('./tweet.html'),
+    createtweet = fs.readFileSync('./createtweet.html'),
+    dbInsertTweet = require('./tweetdb.js').insertTweet,
+    dbDeleteTweet = require('./tweetdb.js').deleteRec,
+    dbSelectTweets = require('./tweetdb.js').selectRecForTweet,  
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.post('/tweet', function(req, res) {    
+    console.log("tweet");
+     var user = {
+        userid: req.body.userid,
+     }       
+     console.log("tweet userid:" + user.userid);  
+     res.setHeader("userid", user.userid)   
+     res.send(createtweet);      
+});
+
+ 
+
+app.post('/createtweet', function(req, res) {
+    console.log("createtweet");
+     
+     var createTweet = {
+        userid: req.header.userid,
+        tweettext: req.body.tweettext,
+     } 
+      console.log("createtweet:" + createTweet.userid + "," + createTweet.tweettext);       
+});
+
+
+app.get('/', function(req, res) {    
+    res.send(html);
+   
+});
+
+
+/*** 
+app.get('/mainApp', function(req, res) {
+    res.send('logged in');
+})
+
+app.get('/adduser', function(req, res) {
+
+    var user = {
+        userid: req.query.userid,
+        password: req.query.password,
+        firstname: req.query.firstname,
+        lastname: req.query.lastname,
+        email: req.query.email
+
+    };
+    var p = dbInsertUser(db, user);
+    p.then(
+        (val) => {
+            res.send('User Added');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+app.get('/addtweet', function(req, res) {
+    var tweet = {
+        userid: req.query.userid,
+        tweetcontent: req.query.tweetcontent,
+        tweetts: new Date()
+    };
+
+    var p = dbInsertTweet(db, tweet);
+    p.then(
+        (val) => {
+            res.send('Tweet Added');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+app.get('/gettweets', function(req, res) {
+    var user = {
+        userid: req.query.userid,
+    };
+
+    var p = dbGetTweets(db, user);
+    p.then(
+        (val) => {
+            res.send(val);
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+*/
+
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!');
+});
--- tweetdb.js
+++ tweetdb.js
@@ -1,6 +1,4 @@
 
-var express = require('express');
-var app = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('test.db');
 
@@ -11,7 +9,7 @@ var db = new sqlite3.Database('test.db');
 //updateAnyColumnWithExistingData("retweeter_userid", 4, 1);
 //updateAnyColumnWithExistingData("userlikeids", 5, 1);
 //selectRecForTweet(5);
-deleteRec(7);
+//deleteRec(7);
 
 function updateTweetwithColumn(column, id, data) {
     db.run("UPDATE tweet SET " + column + "  = ? WHERE tweetid = ?", data, id, function (err) {
@@ -21,9 +19,11 @@ function updateTweetwithColumn(column, id, data) {
         }
         else {
             console.log("Successful");
+            return true;
+            
         }
     });
-    return true;
+    
 }
 
 
@@ -161,5 +161,73 @@ exports.updateAnyColumnWithExistingData = updateAnyColumnWithExistingData;
 exports.selectRec = selectRec;
 exports.insertTweet = insertTweet;
 exports.deleteRec = deleteRec;
+
+
+/*
+
+TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
+var db = new TransactionDatabase(
+    new sqlite3.Database("test.sqlite", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
+);
+
+function updateAnyColumnWithExistingData(columnName, tweetid, userid) {
+    var p;
+    db.beginTransaction(function (err, transaction) {
+        p = new Promise((resolve, reject) => {
+            transaction.each("SELECT " + columnName + " FROM tweet where tweetid = ?", tweetid, function (err, row) {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                resolve(row)
+                console.log("row:" + row[columnName]);
+            })
+        }).then(
+            (row) => {
+                return updateUserLikeIds(transaction, columnName, tweeetid, userid);
+            }
+        ).then(
+            (val) => {
+                if (columnName == "retweeter_userid") {
+                    return updateRetweetBool(transaction, tweetid);
+                }
+            }
+        ).then(
+            (val) => {
+                transaction.commit(function (err) {
+                    console.log('failed to commit');
+                });
+            }
+        ).catch(
+            (err) => {
+                console.log(err);
+                transaction.rollback();
+            }
+        );
+    });
+    return p;
+
+}
+
+
+function updateUserLikeIds(db, columnName, tweetid, userid) {
+    return Promise(function (resolve, reject) {
+        console.log("updateAnyColumnWithExistingData within then: " + row[columnName]);
+        var finalUserids = row[columnName] != null && row[columnName] != "" ? row[columnName] + "," + userid : userid;
+        console.log("updateAnyColumnWithExistingData after adding new user : " + finalUserids);
+        db.run("UPDATE tweet SET " + columnName + " = ? WHERE tweetid = ?", finalUserids, tweetid, function (err) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                console.log("Successful Update userlikeids.");
+                resolve();
+            }
+        });
+    });
+}
+
+*/
 
 

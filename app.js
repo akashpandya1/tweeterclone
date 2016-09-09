6e6feb04ba3b4b16d7471460bd51ae417c88a845
akashpandya1 <akashpandya@gmail.com>
2016-09-09 13:25:12 -0400

    add mode frontend

--- app.js
+++ app.js
@@ -9,106 +9,100 @@ var express = require('express'),
     dbDeleteTweet = require('./tweetdb.js').deleteRec,
     dbSelectTweets = require('./tweetdb.js').selectRecForTweet,  
     bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+  
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
     extended: true
-}));
-
-
+    }));
+    
 app.post('/tweet', function(req, res) {    
     console.log("tweet");
      var user = {
         userid: req.body.userid,
      }       
-     console.log("tweet userid:" + user.userid);  
-     res.setHeader("userid", user.userid)   
-     res.send(createtweet);      
+     console.log("tweet userid:" + user.userid);      
+     res.render('createtweet');     
 });
-
- 
 
 app.post('/createtweet', function(req, res) {
     console.log("createtweet");
      
      var createTweet = {
-        userid: req.header.userid,
-        tweettext: req.body.tweettext,
+        userid: req.body.userid,
+        tweettext: req.body.tweettext
      } 
-      console.log("createtweet:" + createTweet.userid + "," + createTweet.tweettext);       
+
+    console.log("createtweet:" + createTweet.userid + "," + createTweet.tweettext);    
+
+    var p = insertTweet(createTweet);
+    p.then(
+        (val) => {
+            res.send('Tweeted!');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        });        
 });
 
+app.post('/deletetweet', function(req, res) {
+    console.log("deletetweet");
+     
+     var deleteTweet = {
+        tweetid: req.body.tweetid        
+     }; 
+
+    console.log("tweetid:" + deleteTweet.tweetid);    
+
+    var p = deleteRec(deleteTweet.tweetid);
+    p.then(
+        (val) => {
+            res.send('Deleted!');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        });        
+});
+
+
+app.post('/selecttweet', function(req, res) {
+    console.log("selecttweet");
+    var p = selectRec();
+    p.then(
+        (val) => {
+            res.send('selected all tweets!');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        });        
+});
+
+ 
+
+app.post('/selectRecForTweet', function(req, res) {
+    console.log("selectRecForTweet");
+
+     var selectTweetRec = {
+        tweetid: req.body.tweetid        
+     }; 
+    var p = selectRecForTweet(selectTweetRec.tweetid);
+    p.then(
+        (val) => {
+            res.send('selected tweet record!');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        });        
+});
 
 app.get('/', function(req, res) {    
     res.send(html);
    
 });
-
-
-/*** 
-app.get('/mainApp', function(req, res) {
-    res.send('logged in');
-})
-
-app.get('/adduser', function(req, res) {
-
-    var user = {
-        userid: req.query.userid,
-        password: req.query.password,
-        firstname: req.query.firstname,
-        lastname: req.query.lastname,
-        email: req.query.email
-
-    };
-    var p = dbInsertUser(db, user);
-    p.then(
-        (val) => {
-            res.send('User Added');
-        }
-    ).catch(
-        (err) => {
-            res.send(err);
-        }
-    )
-});
-
-app.get('/addtweet', function(req, res) {
-    var tweet = {
-        userid: req.query.userid,
-        tweetcontent: req.query.tweetcontent,
-        tweetts: new Date()
-    };
-
-    var p = dbInsertTweet(db, tweet);
-    p.then(
-        (val) => {
-            res.send('Tweet Added');
-        }
-    ).catch(
-        (err) => {
-            res.send(err);
-        }
-    )
-});
-
-app.get('/gettweets', function(req, res) {
-    var user = {
-        userid: req.query.userid,
-    };
-
-    var p = dbGetTweets(db, user);
-    p.then(
-        (val) => {
-            res.send(val);
-        }
-    ).catch(
-        (err) => {
-            res.send(err);
-        }
-    )
-});
-
-*/
+ 
 
 app.listen(8080, function() {
     console.log('Example app listening on port 8080!');

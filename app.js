3e4e0a177c95519e6e1429c0959f99b247e466b2
akashpandya1 <akashpandya@gmail.com>
2016-09-10 14:28:23 -0400

    modified update column method.

--- app.js
+++ app.js
@@ -7,57 +7,57 @@ var express = require('express'),
     createtweet = fs.readFileSync('./createtweet.html'),
     dbInsertTweet = require('./tweetdb.js').insertTweet,
     dbDeleteTweet = require('./tweetdb.js').deleteRec,
-    dbSelectRecForTweet = require('./tweetdb.js').selectRecForTweet,  
+    dbSelectRecForTweet = require('./tweetdb.js').selectRecForTweet,
     dbAllTweets = require('./tweetdb.js').selectRec
-    bodyParser = require('body-parser');
-    ejs = require('ejs');
-    os = require('os');
+bodyParser = require('body-parser');
+ejs = require('ejs');
+os = require('os');
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
-    }));
-    
-app.post('/tweet', function(req, res) {    
-    console.log("tweet");
-     var user = {
-        userid: req.body.userid,
-     }       
-     console.log("tweet userid:" + user.userid);   
-     fs.readFile('createtweet.html', 'utf-8', function(err, content) {
-         if (err) {
-             res.end('error occurred');
-             return;
-         } 
+}));
 
-     var renderedHtml = ejs.render(content, {userid: user.userid});  
-     console.log("renderedHtml:" + renderedHtml);  
-     res.end(renderedHtml);  
-   });  
+app.post('/tweet', function (req, res) {
+    console.log("tweet");
+    var user = {
+        userid: req.body.userid,
+    }
+    console.log("tweet userid:" + user.userid);
+    fs.readFile('createtweet.html', 'utf-8', function (err, content) {
+        if (err) {
+            res.end('error occurred');
+            return;
+        }
+
+        var renderedHtml = ejs.render(content, { userid: user.userid });
+        console.log("renderedHtml:" + renderedHtml);
+        res.end(renderedHtml);
+    });
 });
 
-app.post('/createtweet', function(req, res) {
+app.post('/createtweet', function (req, res) {
     console.log("createtweet");
-     
-     var createTweet = {
-         tweetText: req.body.tweettext,
-         authorID: req.body.userid        
-     };
-    console.log("createtweet:" + createTweet.userid + "," + createTweet.tweettext);    
-    jSONStr = '[' + JSON.stringify(createTweet) + ']';    
+
+    var createTweet = {
+        tweetText: req.body.tweettext,
+        authorID: req.body.userid
+    };
+    console.log("createtweet:" + createTweet.userid + "," + createTweet.tweettext);
+    jSONStr = '[' + JSON.stringify(createTweet) + ']';
     console.log("jSONStr: " + jSONStr);
     dbInsertTweet(jSONStr);
-    res.send('Tweeted!');        
+    res.send('Tweeted!');
 });
 
-app.post('/deletetweet', function(req, res) {
+app.post('/deletetweet', function (req, res) {
     console.log("deletetweet");
-     
-     var deleteTweet = {
-        tweetid: req.body.tweetid        
-     }; 
 
-    console.log("tweetid:" + deleteTweet.tweetid);    
+    var deleteTweet = {
+        tweetid: req.body.tweetid
+    };
+
+    console.log("tweetid:" + deleteTweet.tweetid);
 
     var p = dbDeleteTweet(deleteTweet.tweetid);
     p.then(
@@ -67,11 +67,11 @@ app.post('/deletetweet', function(req, res) {
     ).catch(
         (err) => {
             res.send(err);
-        });        
+        });
 });
 
 
-app.post('/selecttweet', function(req, res) {
+app.post('/selecttweet', function (req, res) {
     console.log("selecttweet");
     var p = dbAllTweets();
     p.then(
@@ -81,17 +81,17 @@ app.post('/selecttweet', function(req, res) {
     ).catch(
         (err) => {
             res.send(err);
-        });        
+        });
 });
 
- 
 
-app.post('/selectRecForTweet', function(req, res) {
+
+app.post('/selectRecForTweet', function (req, res) {
     console.log("selectRecForTweet");
 
-     var selectTweetRec = {
-        tweetid: req.body.tweetid        
-     }; 
+    var selectTweetRec = {
+        tweetid: req.body.tweetid
+    };
     var p = dbSelectRecForTweet(selectTweetRec.tweetid);
     p.then(
         (val) => {
@@ -100,15 +100,15 @@ app.post('/selectRecForTweet', function(req, res) {
     ).catch(
         (err) => {
             res.send(err);
-        });        
+        });
 });
 
-app.get('/', function(req, res) {    
+app.get('/', function (req, res) {
     res.send(html);
-   
-});
- 
 
-app.listen(8080, function() {
+});
+
+
+app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
 });
